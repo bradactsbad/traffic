4a8abcff56e9f9a1f084f5f1977afb06f43212d3
Bradley Moss <brad@bradmoss.net>
2023-06-10 19:18:05 -0400

    refactor

--- traffic.py
+++ traffic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests
+import pdb
 from bs4 import BeautifulSoup
 
 URL = "https://www.511virginia.org/mobile/?menu_id=Prince%20George%20County&ident=County%7CPrince%20George%20County"
@@ -12,26 +13,14 @@ def get_511(url):
     elements = (("h2", "reportHeading"), ("div", "reportBody"), ("h1", "reportType"))
     page_data = {}
     for i, e in enumerate(elements):
-        page_data[e[1]] = (soup.find_all(e[0], {"class": e[1]}),)
-    data = {}
-    for (
-        k,
-        v,
-    ) in page_data.items():
-        for results in v:
-            for tag in results:
-                if k in data:
-                    data[k].append(tag.text.strip())
-                else:
-                    data[k] = [tag.text.strip()]
+        page_data[e[1]] = soup.find_all(e[0], {"class": e[1]})
     reports = []
-    first_data = next(iter(data.items()))
-    number_of_reports = len(first_data[1])
+    number_of_reports = len(next(iter(page_data.items()))[1])
     for i in range(0, number_of_reports):
-        reports.append(dict())
+        reports.append({})
     for i, report in enumerate(reports):
-        for key, content in data.items():
-            report[key] = content[i]
+        for key, content in page_data.items():
+            report[key] = content[i].text.strip()
     return reports
 
 
