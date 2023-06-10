29cad0451e2db1966566fb5124c29f427eed82a6
Bradley Moss <brad@bradmoss.net>
2023-06-10 19:41:55 -0400

    rescope and refactor. move urls to yaml file

--- traffic.py
+++ traffic.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
-import pdb
 import yaml
 import requests
 from bs4 import BeautifulSoup
 
 
-with open('urls.yaml', 'r') as urls_file:
-    URLS = yaml.safe_load(urls_file)['urls']
-
 def get_511(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "xml")
-    elements = (("div", "reportBody"),)
     reports = soup.find_all('div', {'class': 'reportBody'})
     return [report.text.strip() for report in reports]
+
+with open('urls.yaml', 'r') as urls_file:
+    URLS = yaml.safe_load(urls_file)['urls']
 
 
 if __name__ == "__main__":
