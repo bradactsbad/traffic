dce7bd665bd0970e9726f5fb976a12cdbc53d3eb
Bradley Moss <brad@bradmoss.net>
2023-06-10 19:41:55 -0400

    rescope and refactor. move urls to yaml file

--- traffic.py
+++ traffic.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 
+import pdb
+import yaml
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.511virginia.org/mobile/?menu_id=Prince%20George%20County&ident=County%7CPrince%20George%20County"
 
+with open('urls.yaml', 'r') as urls_file:
+    URLS = yaml.safe_load(urls_file)['urls']
 
 def get_511(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "xml")
-    elements = (("h2", "reportHeading"), ("div", "reportBody"), ("h1", "reportType"))
-    page_data = {}
-    for i, e in enumerate(elements):
-        page_data[e[1]] = soup.find_all(e[0], {"class": e[1]})
-    reports = []
-    number_of_reports = len(next(iter(page_data.items()))[1])
-    for i in range(0, number_of_reports):
-        reports.append({})
-    for i, report in enumerate(reports):
-        for key, content in page_data.items():
-            report[key] = content[i].text.strip()
-    return reports
+    elements = (("div", "reportBody"),)
+    reports = soup.find_all('div', {'class': 'reportBody'})
+    return [report.text.strip() for report in reports]
 
 
 if __name__ == "__main__":
-    reports = get_511(URL)
+    reports = []
+    for url in URLS:
+        reports += get_511(url)
     for report in reports:
-        print(report["reportBody"] + "\n")
+        print(report + '\n')
