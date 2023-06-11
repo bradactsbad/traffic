b43914d298d9f9b8e79fca4f34edcc19c886e182
Bradley Moss <brad@bradmoss.net>
2023-06-11 00:44:43 -0400

    refactor into classes

--- traffic.py
+++ traffic.py
@@ -9,33 +9,58 @@ from concurrent import futures
 from bs4 import BeautifulSoup
 
 
-def get_511(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "xml")
-    reports = soup.find_all("div", {"class": "reportBody"})
-    return [report.text.strip() for report in reports]
+class Traffic:
+    def __init__(self):
+        self.urls_filename = "urls.txt"
+        self.urls = self._load_urls_from_config()
+        self.reports = self._get_reports_from_urls()
+
+    @staticmethod
+    def get_511(url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "xml")
+        reports = soup.find_all("div", {"class": "reportBody"})
+        return [report.text.strip() for report in reports]
+
+    def _load_urls_from_config(self):
+        def urls_filepath(filename):
+            script_path = os.path.dirname(os.path.realpath(__file__))
+            return os.path.join(script_path, filename)
+
+        with open(urls_filepath(self.urls_filename)) as f:
+            urls = f.read().splitlines()
+        return urls
+
+    def _get_reports_from_urls(self):
+        reports = []
+        with futures.ThreadPoolExecutor(max_workers=8) as executor:
+            reports = list(executor.map(self.get_511, self.urls))
+        return reduce(concat, reports)
+
+    def search(self, *terms):
+        results = []
+        for term in terms:
+            results += [report for report in self.reports if term.lower() in report.lower()]  # type: ignore
+        return results
 
 
-def load_urls_from_config():
-    URLS_FILENAME = "urls.txt"
+class Cli:
+    def __init__(self, traffic):
+        self.output = []
+        self.traffic = traffic
+        self.args = self.parse_args()
+        if self.args.bridge:
+            self.bridge()
+        if self.args.rt5:
+            self.rt5()
+        if self.args.location:
+            self.output += self.traffic.search(self.args.location)
+        else:
+            if not self.args.rt5 and not self.args.bridge:
+                self.output = self.traffic.reports
+        self.print_output()
 
-    def urls_filepath(filename):
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        return os.path.join(script_path, filename)
-
-    with open(urls_filepath(URLS_FILENAME)) as f:
-        urls = f.read().splitlines()
-    return urls
-
-
-def get_reports_from_urls(urls):
-    reports = []
-    with futures.ThreadPoolExecutor(max_workers=8) as executor:
-        reports = list(executor.map(get_511, urls))
-    return reduce(concat, reports)
-
-
-def cli():
+    @staticmethod
     def parse_args():
         parser = argparse.ArgumentParser(
             prog="traffic", description="Virginia 511 compatible traffic scraper."
@@ -56,42 +81,28 @@ def cli():
         )
         return parser.parse_args()
 
-    def print_output(output):
-        if len(output) == 0:
+    def bridge(self):
+        TERM = "Benjamin Harrison Bridge"
+        self.output += self.traffic.search(TERM)
+
+    def rt5(self):
+        TERMS = ("rt. 5E/W", "VA-5")
+        results = self.traffic.search(*TERMS)
+        if len(results) == 0:
+            self.output += ["There are no results for VA-5 at this time."]
+        else:
+            self.output += results
+
+    def print_output(self):
+        if len(self.output) == 0:
             print("No results to display.")
         else:
-            for i, line in enumerate(output):
-                if i == len(output) - 1:
+            for i, line in enumerate(self.output):
+                if i == len(self.output) - 1:
                     print(line)
                 else:
                     print(line + "\n")
 
-    def search(reports, *terms):
-        results = []
-        for term in terms:
-            results += [report for report in reports if term.lower() in report.lower()]
-        return results
-
-    args = parse_args()
-    reports = get_reports_from_urls(load_urls_from_config())
-    output = []
-    if args.bridge:
-        TERM = "Benjamin Harrison Bridge"
-        output += search(reports, TERM)
-    if args.rt5:
-        TERMS = ("rt. 5E/W", "VA-5")
-        results = search(reports, *TERMS)
-        if len(results) == 0:
-            output += ["There are no results for VA-5 at this time."]
-        else:
-            output += results
-    if args.location:
-        output += search(args.location, reports)
-    else:
-        if not args.rt5 and not args.bridge:
-            output = reports
-    print_output(output)
-
 
 if __name__ == "__main__":
-    cli()
+    Cli(Traffic())
