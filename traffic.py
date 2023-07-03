388fa95750994582e127fcbcef39d41a4159352b
Bradley Moss <brad@bradmoss.net>
2023-07-03 13:49:32 -0400

    refactor

--- traffic.py
+++ traffic.py
@@ -1,112 +1,82 @@
 #!/usr/bin/env python
 
 import os
-import requests
+import urllib.request
 import argparse
 from functools import reduce
 from operator import concat
 from concurrent import futures
 from bs4 import BeautifulSoup
 
+URLS = """https://www.511virginia.org/mobile/?menu_id=Henrico%20County&ident=County%7CHenrico%20County
+https://www.511virginia.org/mobile/?menu_id=Prince%20George%20County&ident=County%7CPrince%20George%20County
+https://www.511virginia.org/mobile/?menu_id=Richmond&ident=City%7CRichmond
+https://www.511virginia.org/mobile/?menu_id=New%20Kent%20County&ident=County%7CNew%20Kent%20County
+https://www.511virginia.org/mobile/?menu_id=Chesterfield%20County&ident=County%7CChesterfield%20County"""
 
-class Traffic:
-    URLS_FILENAME = "urls.txt"
 
-    def __init__(self):
-        self.urls = self._load_urls_from_config()
-        self.reports = self._get_reports_from_urls()
-
-    @staticmethod
-    def get_511(url):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, "xml")
+def fetch():
+    def fetch(url):
+        with urllib.request.urlopen(url) as resp:
+            soup = BeautifulSoup(resp, "xml")
         reports = soup.find_all("div", {"class": "reportBody"})
         return [report.text.strip() for report in reports]
 
-    def _load_urls_from_config(self):
-        def urls_filepath(filename):
-            script_path = os.path.dirname(os.path.realpath(__file__))
-            return os.path.join(script_path, filename)
-
-        with open(urls_filepath(Traffic.URLS_FILENAME)) as f:
-            urls = f.read().splitlines()
-        return urls
-
-    def _get_reports_from_urls(self):
-        reports = []
-        with futures.ThreadPoolExecutor(max_workers=8) as executor:
-            reports = list(executor.map(self.get_511, self.urls))
-        return reduce(concat, reports)
-
-    def search(self, *terms):
-        results = []
-        for term in terms:
-            results += [report for report in self.reports if term.lower() in report.lower()]  # type: ignore
-        return results
+    with futures.ThreadPoolExecutor(max_workers=8) as executor:
+        reports = list(executor.map(fetch, URLS.splitlines()))
+    return reduce(concat, reports)
 
 
-class Cli:
-    def __init__(self, traffic):
-        self.output = []
-        self.traffic = traffic
-        self.args = self.parse_args()
-        if self.args.bridge:
-            self.bridge()
-        if self.args.rt5:
-            self.rt5()
-        if self.args.search:
-            self.search()
-        else:
-            if not any((self.args.rt5, self.args.bridge)):
-                self.output = self.traffic.reports
-        self.print_output()
+def search(reports, *terms):
+    results = []
+    for term in terms:
+        results += [rep for rep in reports if term.lower() in rep.lower()]  # type: ignore
+    return results
 
-    def search(self):
-        self.output += self.traffic.search(self.args.search)
 
-    @staticmethod
-    def parse_args():
-        parser = argparse.ArgumentParser(
-            prog="traffic", description="Virginia 511 compatible traffic scraper."
-        )
-        parser.add_argument(
-            "--bridge",
-            action="store_true",
-            help="Return available information on the Benjamin Harrison bridge.",
-        )
-        parser.add_argument(
-            "--rt5", action="store_true", help="Return available information on VA-5."
-        )
-        parser.add_argument(
-            "search",
-            nargs="?",
-            default=None,
-            help="Find results including [search]. Case insensitive. Optional. If this and all options are empty, the program returns all data.",
-        )
-        return parser.parse_args()
-
-    def bridge(self):
-        TERM = "Benjamin Harrison Bridge"
-        self.output += self.traffic.search(TERM)
-
-    def rt5(self):
-        TERMS = ("rt. 5E/W", "VA-5")
-        results = self.traffic.search(*TERMS)
+def main():
+    parser = argparse.ArgumentParser(
+        prog="traffic", description="Virginia 511 road information."
+    )
+    parser.add_argument(
+        "--bridge",
+        action="store_true",
+        help="Return available information on the Benjamin Harrison bridge.",
+    )
+    parser.add_argument(
+        "--rt5", action="store_true", help="Return available information on VA-5."
+    )
+    parser.add_argument(
+        "search",
+        nargs="?",
+        default=None,
+        help="Find results including [search]. Case insensitive. Optional. If this and all options are empty, the program returns all data.",
+    )
+    args = parser.parse_args()
+    output = []
+    reports = fetch()
+    if args.bridge:
+        output += search(reports, "Benjamin Harrison Bridge")
+    if args.rt5:
+        results = search(reports, *("rt. 5E/W", "VA-5"))
         if len(results) == 0:
-            self.output += ["There are no results for VA-5 at this time."]
+            output += ["There are no results for VA-5 at this time."]
         else:
-            self.output += results
-
-    def print_output(self):
-        if len(self.output) == 0:
-            print("No results to display.")
-        else:
-            for i, line in enumerate(self.output):
-                if i == len(self.output) - 1:
-                    print(line)
-                else:
-                    print(line + "\n")
+            output += results
+    if args.search:
+        output += search(reports, args.search)
+    else:
+        if not any((args.rt5, args.bridge)):
+            output = reports
+    if len(output) == 0:
+        print("No results to display.")
+    else:
+        for i, line in enumerate(output):
+            if i == len(output) - 1:
+                print(line)
+            else:
+                print(line + "\n")
 
 
 if __name__ == "__main__":
-    Cli(Traffic())
+    main()
